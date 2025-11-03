5123a886c81fc12545e902aa6aa539b18a060150
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-03 12:49:50 +0700

    modified index.ts to .tsx and deleted pages/index.tsx to avoid routing conflics

--- alx-project-0x03/app/layout.tsx
+++ alx-project-0x03/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// Concerning this css error:
+// TypeScript is for types and syntax: It checks JavaScript/TypeScript code.
+// Whereas css is not code, it is a stylesheet
+// FIX: here we should tell TS how to handle css files using another file like custom.d.ts 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
