da1793e416a916515d2a3d3dc4848eebfbad8e51
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-03 12:54:50 +0700

    modifying the pages/index.tsx to import the layout and insert it

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,8 +1,17 @@
+// 1. Import the custom Layout component from components directory.
+// The refactoring step is to import custom <Layout> component 
+// and wrap the page content with it to apply the shared header and footer.
+
+import Layout from '../components/layouts/Layout';
+
 const Home = () => {
     return (
-     <div>
-      <h1> Welcome to Splash App </h1>
-     </div>)
+     <Layout>
+        <div>
+                  <h1> Welcome to Splash App </h1>
+        </div>
+
+     </Layout>)
   }
   
   export default Home;
